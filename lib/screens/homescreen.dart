063dc74d155acb99d6299b16eaeee9de9934a394
Raihan Firdaus <r1aihan2003f1irdaus@gmail.com>
2024-02-08 14:45:15 +0700

    homescreen

---
+++ lib/screens/homescreen.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:todo_app/screens/todo_screen.dart';
+
+class Homescreen extends StatelessWidget {
+  const Homescreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Container(
+      decoration: const BoxDecoration(color: Colors.white),
+      child: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            const Image(
+              image: AssetImage('assets/icon/homescreen-icon.jpg'),
+            ),
+            Text(
+              "Simplify your day with TaskMaster - your go-to app for staying on top of tasks!",
+              style: Theme.of(context).textTheme.titleLarge!.copyWith(
+                    color: Theme.of(context).colorScheme.primary,
+                  ),
+              textAlign: TextAlign.center,
+            ),
+            ElevatedButton(
+                onPressed: () {
+                  Navigator.of(context).push(
+                    MaterialPageRoute(builder: (context) => const TodoScreen()),
+                  );
+                },
+                child: Text(
+                  "Next",
+                  style: Theme.of(context).textTheme.titleMedium!.copyWith(
+                        color: Theme.of(context).colorScheme.primary,
+                      ),
+                ))
+          ],
+        ),
+      ),
+    );
+  }
+}
